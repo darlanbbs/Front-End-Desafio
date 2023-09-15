70440fc952ac759c5d4429532abd4d232f645987
Darlan Bomfim <darlanbs05@gmail.com>
2023-09-15 15:38:31 -0300

    Finalização modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import * as C from "./styles";
 
 type childrenProps = {
   children: React.ReactNode;
@@ -14,26 +15,23 @@ function ModalComponent({ children, bimestre }: childrenProps, args: any) {
   return (
     <div>
       <div onClick={toggle}>{children}</div>
-      <Modal isOpen={modal} toggle={toggle} {...args}>
+      <C.Modal isOpen={modal} toggle={toggle} {...args}>
         <ModalHeader toggle={toggle}>{bimestre}</ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <C.DisciplinaDiv>Disciplina</C.DisciplinaDiv>
+          <C.ButtonsContainer>
+            <C.StyledButton color="info">Sociologia</C.StyledButton>
+            <C.StyledButton color="success">Biologia</C.StyledButton>
+            <C.StyledButton color="warning">Geografia</C.StyledButton>
+            <C.StyledButton color="danger">Artes</C.StyledButton>
+          </C.ButtonsContainer>
+          <C.NotasDiv>Notas</C.NotasDiv>
+          <C.StyledInput type="number" min="0" max="10" />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
+          <C.StyledButton onClick={toggle}>Confirmar</C.StyledButton>
         </ModalFooter>
-      </Modal>
+      </C.Modal>
     </div>
   );
 }
--- src/components/modal/styles.tsx
+++ src/components/modal/styles.tsx
@@ -1,1 +1,40 @@
 import styled from "styled-components";
+import { Button as RSButton, Modal as RSModal } from "reactstrap";
+
+export const DisciplinaDiv = styled.div`
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const NotasDiv = styled.div`
+  font-weight: bold;
+  margin-top: 20px;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const StyledInput = styled.input`
+  width: 50px;
+  padding: 5px;
+  font-size: 16px;
+`;
+
+export const StyledButton = styled(RSButton)`
+  background-color: #e9ff1a;
+  border: none;
+  color: black;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+export const Modal = styled(RSModal)`
+  .modal-content {
+    background-color: #fff;
+    color: #000;
+  }
+`;
